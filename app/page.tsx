2cd29439d96edd35d9882a0015a6dddd6709ba02
abanero1998-afk <abanero1998@icloud.com>
2026-09-10 00:57:36 +0200

    Splash lives inside map boot

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import dynamic from "next/dynamic";
 import RegisterSW from "@/components/RegisterSW";
-import Splash from "@/components/Splash";
 
 const DottorSOS = dynamic(() => import("@/components/DottorSOS"), {
   ssr: false,
@@ -8,6 +7,7 @@ const DottorSOS = dynamic(() => import("@/components/DottorSOS"), {
     <div className="w-full h-screen bg-[#0b0b0b] text-white flex flex-col items-center justify-center">
       <img src="/logo.svg" alt="DOTTOR SOS" className="w-28 h-28 rounded-[28px]" />
       <p className="mt-4 font-black text-2xl">DOTTOR SOS</p>
+      <p className="text-white/50 mt-2">0%</p>
     </div>
   ),
 });
@@ -16,7 +16,6 @@ export default function Page() {
   return (
     <>
       <RegisterSW />
-      <Splash />
       <DottorSOS />
     </>
   );
